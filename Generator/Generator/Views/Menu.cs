dea9c269f24d02672ac7f39eaa6b4cee4faa2826
osaama05 <oskari.makela@hotmail.com>
2023-05-23 23:48:26 +0300

    Added choices for combobox

--- Generator/Generator/Views/Menu.cs
+++ Generator/Generator/Views/Menu.cs
@@ -1,10 +1,27 @@
 namespace Generator
 {
-	public partial class Menu : Form
-	{
-		public Menu()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Menu : Form
+    {
+        private readonly List<string> searchOptions = new();
+        public Menu()
+        {
+            InitializeComponent();
+            InitializeSearch();
+        }
+
+        private void InitializeSearch()
+        {
+            searchOptions.Add("Short Answer");
+            searchOptions.Add("Short Answer (Case sensitive)");
+            searchOptions.Add("Dropdown menu in-line in the text");
+            searchOptions.Add("Vertical column of radio buttons");
+            searchOptions.Add("Horizontal row of radio-buttons");
+            searchOptions.Add("Numerical");
+
+            cmbSelect.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            cmbSelect.DataSource = searchOptions;
+
+        }
+    }
 }

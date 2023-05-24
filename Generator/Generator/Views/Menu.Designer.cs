fc1d3d2b00100bde6e68f1498ab79a26969941c1
osaama05 <oskari.makela@hotmail.com>
2023-05-24 13:35:34 +0300

    Better coloring

--- Generator/Generator/Views/Menu.Designer.cs
+++ Generator/Generator/Views/Menu.Designer.cs
@@ -1,106 +1,106 @@
 ﻿namespace Generator
 {
-    partial class Menu
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Menu
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            cmbSelect = new ComboBox();
-            lblSelect = new Label();
-            btnEncode = new Button();
-            txtBoxOutput = new TextBox();
-            lblOutput = new Label();
-            SuspendLayout();
-            // 
-            // cmbSelect
-            // 
-            cmbSelect.FormattingEnabled = true;
-            cmbSelect.Location = new Point(12, 38);
-            cmbSelect.Name = "cmbSelect";
-            cmbSelect.Size = new Size(265, 23);
-            cmbSelect.TabIndex = 0;
-            // 
-            // lblSelect
-            // 
-            lblSelect.AutoSize = true;
-            lblSelect.Location = new Point(12, 20);
-            lblSelect.Name = "lblSelect";
-            lblSelect.Size = new Size(124, 15);
-            lblSelect.TabIndex = 1;
-            lblSelect.Text = "Valitse kysymysmuoto";
-            // 
-            // btnEncode
-            // 
-            btnEncode.BackColor = Color.White;
-            btnEncode.Location = new Point(12, 303);
-            btnEncode.Name = "btnEncode";
-            btnEncode.Size = new Size(75, 23);
-            btnEncode.TabIndex = 2;
-            btnEncode.Text = "Luo";
-            btnEncode.UseVisualStyleBackColor = false;
-            // 
-            // txtBoxOutput
-            // 
-            txtBoxOutput.Location = new Point(12, 359);
-            txtBoxOutput.Name = "txtBoxOutput";
-            txtBoxOutput.Size = new Size(376, 23);
-            txtBoxOutput.TabIndex = 3;
-            // 
-            // lblOutput
-            // 
-            lblOutput.AutoSize = true;
-            lblOutput.Location = new Point(12, 341);
-            lblOutput.Name = "lblOutput";
-            lblOutput.Size = new Size(51, 15);
-            lblOutput.TabIndex = 4;
-            lblOutput.Text = "Ulostulo";
-            // 
-            // Menu
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.LightGray;
-            ClientSize = new Size(800, 450);
-            Controls.Add(lblOutput);
-            Controls.Add(txtBoxOutput);
-            Controls.Add(btnEncode);
-            Controls.Add(lblSelect);
-            Controls.Add(cmbSelect);
-            Name = "Menu";
-            Text = "Main Menu";
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			cmbSelect = new ComboBox();
+			lblSelect = new Label();
+			btnEncode = new Button();
+			txtBoxOutput = new TextBox();
+			lblOutput = new Label();
+			SuspendLayout();
+			// 
+			// cmbSelect
+			// 
+			cmbSelect.FormattingEnabled = true;
+			cmbSelect.Location = new Point(12, 38);
+			cmbSelect.Name = "cmbSelect";
+			cmbSelect.Size = new Size(265, 23);
+			cmbSelect.TabIndex = 0;
+			// 
+			// lblSelect
+			// 
+			lblSelect.AutoSize = true;
+			lblSelect.Location = new Point(12, 20);
+			lblSelect.Name = "lblSelect";
+			lblSelect.Size = new Size(124, 15);
+			lblSelect.TabIndex = 1;
+			lblSelect.Text = "Valitse kysymysmuoto";
+			// 
+			// btnEncode
+			// 
+			btnEncode.BackColor = SystemColors.ControlLightLight;
+			btnEncode.Location = new Point(12, 303);
+			btnEncode.Name = "btnEncode";
+			btnEncode.Size = new Size(75, 23);
+			btnEncode.TabIndex = 2;
+			btnEncode.Text = "Luo";
+			btnEncode.UseVisualStyleBackColor = false;
+			// 
+			// txtBoxOutput
+			// 
+			txtBoxOutput.Location = new Point(12, 359);
+			txtBoxOutput.Name = "txtBoxOutput";
+			txtBoxOutput.Size = new Size(376, 23);
+			txtBoxOutput.TabIndex = 3;
+			// 
+			// lblOutput
+			// 
+			lblOutput.AutoSize = true;
+			lblOutput.Location = new Point(12, 341);
+			lblOutput.Name = "lblOutput";
+			lblOutput.Size = new Size(51, 15);
+			lblOutput.TabIndex = 4;
+			lblOutput.Text = "Ulostulo";
+			// 
+			// Menu
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.FromArgb(212, 207, 201);
+			ClientSize = new Size(800, 450);
+			Controls.Add(lblOutput);
+			Controls.Add(txtBoxOutput);
+			Controls.Add(btnEncode);
+			Controls.Add(lblSelect);
+			Controls.Add(cmbSelect);
+			Name = "Menu";
+			Text = "Main Menu";
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private ComboBox cmbSelect;
-        private Label lblSelect;
-        private Button btnEncode;
-        private TextBox txtBoxOutput;
-        private Label lblOutput;
-    }
+		private ComboBox cmbSelect;
+		private Label lblSelect;
+		private Button btnEncode;
+		private TextBox txtBoxOutput;
+		private Label lblOutput;
+	}
 }
--- Generator/Generator/Views/Menu.cs
+++ Generator/Generator/Views/Menu.cs
@@ -1,27 +1,27 @@
 namespace Generator
 {
-    public partial class Menu : Form
-    {
-        private readonly List<string> searchOptions = new();
-        public Menu()
-        {
-            InitializeComponent();
-            InitializeSearch();
-        }
+	public partial class Menu : Form
+	{
+		private readonly List<string> searchOptions = new();
+		public Menu()
+		{
+			InitializeComponent();
+			InitializeSearch();
+		}
 
-        private void InitializeSearch()
-        {
-            searchOptions.Add("Short Answer");
-            searchOptions.Add("Short Answer (Case sensitive)");
-            searchOptions.Add("Dropdown menu in-line in the text");
-            searchOptions.Add("Vertical column of radio buttons");
-            searchOptions.Add("Horizontal row of radio-buttons");
-            searchOptions.Add("Numerical");
+		private void InitializeSearch()
+		{
+			searchOptions.Add("Short Answer");
+			searchOptions.Add("Short Answer (Case sensitive)");
+			searchOptions.Add("Dropdown menu in-line in the text");
+			searchOptions.Add("Vertical column of radio buttons");
+			searchOptions.Add("Horizontal row of radio-buttons");
+			searchOptions.Add("Numerical");
 
-            cmbSelect.DropDownStyle = ComboBoxStyle.DropDownList;
+			cmbSelect.DropDownStyle = ComboBoxStyle.DropDownList;
 
-            cmbSelect.DataSource = searchOptions;
+			cmbSelect.DataSource = searchOptions;
 
-        }
-    }
+		}
+	}
 }
